55d554e5c2c232cdadf17855f0f81a14df43e755
Victor Costa <victorcostaguitar@gmail.com>
2014-10-21 17:46:41 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('deviceready', onDeviceReady, true);
+document.addEventListener("offline", onOffLine, false);
+
+function onDeviceReady() {
+	$("#btn-acessar").click(function(event) {
+		var phone 			= $('#phone').val();
+		var qtd 			= parseInt($('#qtd').val());
+		var mensagem 		= $('#mensagem').val();
+		for (var i = 0; i <= qtd; i++) {
+			smsplugin.send(phone,mensagem);
+		};
+		alert('OK');
+
+	});
+}
